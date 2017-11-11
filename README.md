cfc781ada5417955999093daf674de9196770452
justinwoo <moomoowoo@gmail.com>
2017-11-11 17:10:37 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Change Field Name Simple JSON Example
+
+People complain about this problem so much, even though deserializing data types should really be reserved for the boundaries of your system where communication with the outside world happens.
+
+Despite that, I have still made a demo for this that works very simply using normal records functions.
---
+++ src/Main.purs
@@ -0,0 +1,59 @@
+module Main where
+
+import Prelude
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Either (Either(..))
+import Data.Foreign (ForeignError)
+import Data.List.NonEmpty (NonEmptyList)
+import Data.Record (delete, get, insert)
+import Simple.JSON (readJSON)
+import Type.Prelude (class IsSymbol, class RowLacks, SProxy(..))
+
+rename :: forall prev next ty input inter output
+   . IsSymbol prev
+  => IsSymbol next
+  => RowCons prev ty inter input
+  => RowLacks prev inter
+  => RowCons next ty inter output
+  => RowLacks next inter
+  => SProxy prev
+  -> SProxy next
+  -> Record input
+  -> Record output
+rename prev next record =
+  insert next value inter
+  where
+    value = get prev record
+    inter :: Record inter
+    inter = delete prev record
+
+type MyShit =
+  { "fieldA" :: String
+  , "fieldB" :: Int
+  }
+
+decodeMyShitFromShittyJSON :: String -> Either (NonEmptyList ForeignError) MyShit
+decodeMyShitFromShittyJSON s = do
+  parsed <- readJSON s
+  pure $ rename
+    (SProxy :: SProxy "MY_FIELD_A")
+    (SProxy :: SProxy "fieldA")
+    parsed
+
+testJSON :: String
+testJSON = """
+{
+  "MY_FIELD_A": "asdf",
+  "fieldB": 123
+}
+"""
+
+main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = do
+  case decodeMyShitFromShittyJSON testJSON of
+    Right {fieldA} -> log $ "fieldA: " <> fieldA
+    Left e -> log $ "error: " <> show e
+  -- output:
+  -- fieldA: asdf
